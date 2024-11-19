180b813317323d3a44257a0bd8c81088a276637c
dAsHMeLnyK <daryna.melnyk@oa.edu.ua>
2024-11-19 19:43:54 +0200

    refactor

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -27,7 +27,7 @@ const VideoDetail = () => {
   return (
     <Box minHeight="95vh">
       <Stack direction={{ xs: 'column', md: 'row' }}>
-        <Box felx={1}>
+        <Box flex={1}>
           <Box sx={{ width: '100%', position: 'sticky', top: '86px'}}>
             <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className="react-player" controls />
             <Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
