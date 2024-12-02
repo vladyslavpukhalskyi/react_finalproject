b56cc8e3d601a8956c0378b7c75922d42b0aa560
dAsHMeLnyK <daryna.melnyk@oa.edu.ua>
2024-12-02 18:06:24 +0200

    Scroll

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -39,7 +39,7 @@ const Feed = () => {
         </Typography>
       </Box>
 
-      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+      <Box p={2} >
         <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: 'white' }}>
           {selectedCategory} <span style={{ color: '#F31503' }}>videos</span>
         </Typography>
