e5acce6ac5e6e46cb4565a57dc251c7c7208735d
dAsHMeLnyK <daryna.melnyk@oa.edu.ua>
2024-12-16 12:06:27 +0200

    Layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Box } from '@mui/material';
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
 
-import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from './common';
+import Layout from "./common/Layout";
+import { Feed, VideoDetail, ChannelDetail, SearchFeed } from "./common";
 
 const App = () => {
-    return (
-      <BrowserRouter>
-        <Box sx={{ backgroundColor: '#000' }}>
-          <Navbar />
-          <Routes>
-            <Route path="/" exact element={<Feed />} />
-            <Route path="/video/:id" element={<VideoDetail />} />
-            <Route path="/channel/:id" element={<ChannelDetail />} />
-            <Route path="/search/:searchTerm" element={<SearchFeed />} />
-            <Route path="/category/:categoryName" element={<Feed />} />
-          </Routes>
-        </Box>
-      </BrowserRouter>
-    );
-  };
+  const [selectedCategory, setSelectedCategory] = useState("New");
 
-export default App
+  return (
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: "#000" }}>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Layout
+                selectedCategory={selectedCategory}
+                setSelectedCategory={setSelectedCategory}
+              />
+            }
+          >
+            <Route
+              index
+              element={
+                <Feed
+                  selectedCategory={selectedCategory}
+                  setSelectedCategory={setSelectedCategory}
+                />
+              }
+            />
+            <Route path="video/:id" element={<VideoDetail />} />
+            <Route path="channel/:id" element={<ChannelDetail />} />
+            <Route path="search/:searchTerm" element={<SearchFeed />} />
+            <Route
+              path="category/:categoryName"
+              element={
+                <Feed
+                  selectedCategory={selectedCategory}
+                  setSelectedCategory={setSelectedCategory}
+                />
+              }
+            />
+          </Route>
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  );
+};
+
+export default App;
